95644a108926d4ce940ed823fca21121d16169c2
Vincent Brodin <vincent.brodin21@gmail.com>
2025-05-23 14:04:26 +0200

    Searchable lists compelted

--- example/main.go
+++ example/main.go
@@ -6,20 +6,13 @@ import (
 )
 
 func main() {
-	items := []string{"Test", "Hello", "Cool", "World", "Golang", "House", "Boat", "Other stuff"}
+	items := []string{"Test","tuff", "Hello", "Cool", "World", "Golang", "House", "Boat", "Other stuff"}
 
 	list := whale.NewList(items)
-
-	// for {
-	// 	key, _ := list.Screen.ReadKey()
-	// 	if key == "ctrl+c" {
-	// 		break
-	// 	}
-	// }
-
 	i, err := list.Prompt("Select something")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(items)
 	fmt.Println(items[i])
 }
